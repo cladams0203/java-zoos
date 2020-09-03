7a0ce4a2003e434c222cfc36d67d1d7ff6131d7d
cladams0203 <cladams0203@gmail.com>
2020-09-03 09:28:21 -0400

    finished the zooanimals model

--- src/main/java/com/lambdaschool/zoos/models/ZooAnimals.java
+++ src/main/java/com/lambdaschool/zoos/models/ZooAnimals.java
@@ -1,10 +1,47 @@
 package com.lambdaschool.zoos.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "zooanimals")
 public class ZooAnimals {
+
+    @Id
+    @ManyToOne
+    @JsonIgnoreProperties(value = "animals")
+    @JoinColumn(name = "zooid")
+    private Zoos zoo;
+
+    @Id
+    @ManyToOne
+    @JsonIgnoreProperties(value = "zoos")
+    @JoinColumn(name = "animalid")
+    private Animals animal;
+
+    public ZooAnimals() {
+    }
+
+    public ZooAnimals(Zoos zoo, Animals animal) {
+        this.zoo = zoo;
+        this.animal = animal;
+    }
+
+    public Zoos getZoo() {
+        return zoo;
+    }
+
+    public void setZoo(Zoos zoo) {
+        this.zoo = zoo;
+    }
+
+    public Animals getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animals animal) {
+        this.animal = animal;
+    }
 }
