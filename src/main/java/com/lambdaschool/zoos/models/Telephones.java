02f5b43e5bf54eea589ee79193318c7ae22b02ff
cladams0203 <cladams0203@gmail.com>
2020-09-03 09:23:16 -0400

    finished telephones model

--- src/main/java/com/lambdaschool/zoos/models/Telephones.java
+++ src/main/java/com/lambdaschool/zoos/models/Telephones.java
@@ -1,4 +1,67 @@
 package com.lambdaschool.zoos.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "telephones")
 public class Telephones {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long phoneid;
+
+    private String phonenumber;
+
+    private String phonetype;
+
+    @ManyToOne
+
+    @JoinColumn(name = "zooid")
+    @JsonIgnoreProperties(value = "telephones", allowSetters = true)
+    private Zoos zoo;
+
+    public Telephones() {
+    }
+
+    public Telephones(long phoneid, String phonenumber, String phonetype, Zoos zoo) {
+        this.phoneid = phoneid;
+        this.phonenumber = phonenumber;
+        this.phonetype = phonetype;
+        this.zoo = zoo;
+    }
+
+    public long getPhoneid() {
+        return phoneid;
+    }
+
+    public void setPhoneid(long phoneid) {
+        this.phoneid = phoneid;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getPhonetype() {
+        return phonetype;
+    }
+
+    public void setPhonetype(String phonetype) {
+        this.phonetype = phonetype;
+    }
+
+    public Zoos getZoo() {
+        return zoo;
+    }
+
+    public void setZoo(Zoos zoo) {
+        this.zoo = zoo;
+    }
 }
